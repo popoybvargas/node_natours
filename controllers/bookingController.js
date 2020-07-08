102b3133f209210468ba240494e2be08c35e1f2d
Popoy Vargas <popoyvargas@me.com>
2020-07-08 17:48:21 +0800

    Fix bug in creating a booking 2.

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -55,7 +55,7 @@ exports.createBookingCheckout = catchAsync( async ( req, res, next ) =>
  {
 	const tour = session.client_reference_id;
 	const user = ( await User.findOne( { email: session.customer_email } ) ).id;
-	const price = session.display_items[ 0 ].amount / 100;
+	const price = session.display_items[ 0 ].amount / 100;	// previously sent line_items property was changed to display_items
 	await Booking.create( { tour, user, price } );
  });
 
